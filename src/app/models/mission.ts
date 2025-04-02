982fa4dd1bd562019275aae76daa1556694e709f
kabamehmetali <mehmetalikabaa@icloud.com>
2025-04-02 10:25:56 -0400

    end

--- src/app/models/mission.ts
+++ src/app/models/mission.ts
@@ -1,18 +1,23 @@
 // File: src/app/models/mission.ts
 export interface Mission {
-    flight_number: number;
-    mission_name: string;
-    launch_year: string;
-    details: string;
-    links: {
-      mission_patch_small: string;
-      article_link: string;
-      wikipedia: string;
-      video_link: string;
-    };
-    rocket: {
-      rocket_name: string;
-      rocket_type: string;
-    };
-  }
-  
+  flight_number: number;
+  mission_name: string;
+  mission_id?: string[];       
+  launch_year: string;
+  launch_success?: boolean;    
+  land_success?: boolean;     
+  details: string;
+  links: {
+    mission_patch_small: string;
+    article_link: string;
+    wikipedia: string;
+    video_link: string;
+  };
+  rocket: {
+    rocket_name: string;
+    rocket_type: string;
+  };
+  launch_site?: {              
+    site_name_long: string;
+  };
+}
